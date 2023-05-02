b5eefac2d7549e08d5b69299d6b02471856fb266
Velinov <124807884+TsvetanVelinov@users.noreply.github.com>
2023-05-02 22:55:23 +0300

    CinemaTicket
    Task for Cinema Ticket

---
+++ 8CinemaTicket.js
@@ -0,0 +1,19 @@
+function cinemaTicket (input){
+
+let day = (input[0]);
+
+if (day === "Monday" || day === "Tuesday" || day === "Friday" ){
+console.log(12);
+} else if (day === "Wednesday" || day === "Thursday" ){
+    console.log(14); 
+} else {
+
+    console.log(16);
+}
+
+}
+
+
+cinemaTicket (["Sunday"])
+cinemaTicket (["Monday"])
+cinemaTicket (["Friday"])
